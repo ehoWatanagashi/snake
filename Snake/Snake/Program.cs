7b1fd8b1849f1666863a6bdab75b26d3f2c2a56d
Artem Pleshkun <asperatus@inbox.ru>
2020-06-04 18:29:07 +0300

    Конструкторы, инкапсуляция

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -4,40 +4,24 @@ namespace Snake
 {
     class Program
     {
-/*        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }*/
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            //Проблемы с кодировкой. Вместо русских символов знаки "?". 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Point p1 = new Point(1,3,'*');
+            /*p1.x = 1;
             p1.y = 3;
-            p1.sym = '*';
+            p1.sym = '*';*/
             p1.Draw();
 
-            /*Draw(p1.x, p1.y, p1.sym);*/
 
-            Point p2 = new Point();
-            p2.x = 4;
+            Point p2 = new Point(4,5,'#');
+/*            p2.x = 4;
             p2.y = 5;
-            p2.sym = '#';
+            p2.sym = '#';*/
             p2.Draw();
 
-            /* Draw(p2.x, p2.y, p2.sym);*/
-
-            /* int x1 = 1;
-             int y1 = 3;
-             char sym1 = '*';
-
-             Draw(x1, y1, sym1);
-
-             int x2 = 4;
-             int y2 = 5;
-             char sym2= '#';
-
-             Draw(x2, y2, sym2);*/
 
             Console.ReadLine();
         }
