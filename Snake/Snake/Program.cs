20b214df5b803fa07a41b9685c2627dd50afa844
Artem Pleshkun <asperatus@inbox.ru>
2020-06-05 18:24:03 +0300

    Списки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -10,19 +11,35 @@ namespace Snake
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             Point p1 = new Point(1,3,'*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.Draw();
 
 
             Point p2 = new Point(4,5,'#');
-/*            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
 
+            //создание списка (нужно подключить директиву using System.Collections.Generic;)
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            //вывод всех значений списка
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            //удаление значений
+            numList.RemoveAt(0);
+
+            //список, содержащий точки
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
             Console.ReadLine();
         }
     }
